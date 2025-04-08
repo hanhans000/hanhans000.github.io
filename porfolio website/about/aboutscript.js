64d41b54ae1619ee9c02fd8737179976d5b82ffc
hanhans000 <baohannguyenngocgrr@gmail.com>
2025-04-08 14:13:55 +0700

    update

---
+++ porfolio website/about/aboutscript.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    // Make the popup draggable
+    $("#popup-container-1").draggable();
+  
+    // Close the popup when clicking on the close button
+    $("#close-btn-1").click(function () {
+      $("#popup-container-1").hide();
+    });
+  });
+  
+  $(document).ready(function () {
+    // Make the popup draggable
+    $("#popup-container-2").draggable();
+  
+    // Close the popup when clicking on the close button
+    $("#close-btn-2").click(function () {
+      $("#popup-container-2").hide();
+    });
+  });
+  
+  $(document).ready(function () {
+    // Make the popup draggable
+    $("#popup-container-3").draggable();
+  
+    // Close the popup when clicking on the close button
+    $("#close-btn-3").click(function () {
+      $("#popup-container-3").hide();
+    });
+  });
+  
+  $(document).ready(function () {
+    // Make the popup draggable
+    $("#picture-1").draggable();
+  
+    // Close the popup when clicking on the close button
+    $("#close-btn-4").click(function () {
+      $("#picture-1").hide();
+    });
+  });
+  
